f3e314574ee60ff60321d023932b97c68e162a03
maxdevback <zacharchuk.max.dev@gmail.com>
2023-10-05 10:55:29 +0300

    Deleted the microservice account port

--- apps/account/src/main.ts
+++ apps/account/src/main.ts
@@ -12,11 +12,8 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const globalPrefix = 'api';
   app.setGlobalPrefix(globalPrefix);
-  const port = process.env.PORT || 4000;
-  await app.listen(port);
-  Logger.log(
-    `🚀 Application is running on: http://localhost:${port}/${globalPrefix}`
-  );
+  await app.init();
+  Logger.log(`🚀 Application is running`);
 }
 
 bootstrap();
